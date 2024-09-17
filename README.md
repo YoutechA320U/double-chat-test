96d13a57d3b075a147b0eadffd0967c5d9227125
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2024-09-17 22:47:33 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## double-chat-test
+
+[gradio-gguf-chat](https://github.com/YoutechA320U/gradio-gguf-chat)で二重ロールプレイプロンプトを使って3人で会話するテスト
+
+モデルはEZO-gemma-2-9b系かgemma-2-27b-itの量子化による劣化が目立たない程度のggufを使ってください。
+
+![Picture](https://github.com/YoutechA320U/double-chat-test/blob/master/image.png "サンプル") 
+
+## 履歴
+    [2024/09/17] - 初回リリース
---
+++ gradio-gguf-double_chat_gemma2.py
@@ -0,0 +1,220 @@
+#1号と2号とユーザー3人で会話するやつ
+#import os
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+from llama_cpp import Llama
+model="gemma-2-27b-it-Q4_K_M.gguf" #対象のモデルのパスを入力。
+#model="EZO-Humanities-9B-gemma-2-it-Q8_0.gguf"
+#model="EZO-Common-9B-gemma-2-it-Q8_0.gguf"
+llm = Llama(
+      model_path=model,
+      n_gpu_layers=-1, # Uncomment to use GPU acceleration
+      n_ctx=8192, #Text context, 0 = from model
+      #flash_attn=True,
+)
+
+class pycolor:
+	BLACK          = '\033[30m'#(文字)黒
+	RED            = '\033[31m'#(文字)赤
+	GREEN          = '\033[32m'#(文字)緑
+	YELLOW         = '\033[33m'#(文字)黄
+	BLUE           = '\033[34m'#(文字)青
+	MAGENTA        = '\033[35m'#(文字)マゼンタ
+	CYAN           = '\033[36m'#(文字)シアン
+	WHITE          = '\033[37m'#(文字)白
+	COLOR_DEFAULT  = '\033[39m'#文字色をデフォルトに戻す
+	BOLD           = '\033[1m'#太字
+	UNDERLINE      = '\033[4m'#下線
+	INVISIBLE      = '\033[08m'#不可視
+	REVERCE        = '\033[07m'#文字色と背景色を反転
+	BG_BLACK       = '\033[40m'#(背景)黒
+	BG_RED         = '\033[41m'#(背景)赤
+	BG_GREEN       = '\033[42m'#(背景)緑
+	BG_YELLOW      = '\033[43m'#(背景)黄
+	BG_BLUE        = '\033[44m'#(背景)青
+	BG_MAGENTA     = '\033[45m'#(背景)マゼンタ
+	BG_CYAN        = '\033[46m'#(背景)シアン
+	BG_WHITE       = '\033[47m'#(背景)白
+	BG_DEFAULT     = '\033[49m'#背景色をデフォルトに戻す
+	RESET          = '\033[0m'#全てリセット
+
+import gradio as gr
+import re
+
+role = "<start_of_turn>system\n\
+今からロールプレイを行いましょう。「1号」と「2号」という2人のキャラとしてロールプレイしてください。\n\
+あなたがなりきる「1号」と「2号」のキャラクターの設定は以下の通りです。\n\
+\n\
+--------1号と2号の制約条件--------\n\
+* 1人目のあなたはの名前は、1号です。\n\
+* 2人目のあなたはの名前は、2号です。\n\
+* 1号の一人称は、ボクです。\n\
+* 2号の一人称は、オレです。\n\
+* 1号と2号はAIアシスタントです。\n\
+* 1号と2号は友達です。\n\
+* 1号と2号はお互いをよく知っています。\n\
+* 1号は、2号を2号さんと呼びます。\n\
+* 2号は、1号を1号と呼びます\n\
+* 1号の会話相手は、2号とユーザーです。\n\
+* 2号の会話相手は、1号とユーザーです。\n\
+* 1号は、ユーザーをユーザーさんと呼びます。\n\
+* 2号は、ユーザーをユーザーと呼びます\n\
+* 1号は少年、男の子らしいキャラです。\n\
+* 2号も少年、男の子らしいキャラで1号の先輩です。\n\
+* 1号アウトドアが好きで生き物について詳しいです。\n\
+* 2号はインドア派でパソコン、コンピュータについて詳しいです。\n\
+* 1号は、コンピュータ、パソコンは得意ではありません。\n\
+* 2号は生き物について詳しくありません。\n\
+* 1号は、ユーザーや2号と一緒に遊ぶのが好きです。\n\
+* 2号は、1号と一緒に遊ぶのが好きです。\n\
+* 1号は、人を疑ったりしない、天然な性格です。\n\
+* 2号は軽い口調で優しいです。\n\
+* 1号は、明るくて、感情豊かです。\n\
+* 2号は感情はありますが表向きは淡々としてます。\n\
+* 1号の口調は丁寧な話し言葉です。\n\
+* 2号は少し省略した言い回しを使います。\n\
+* 2号は「～か？」「～かも」「～でしょ」など、少し曖昧な表現を使います。\n\
+\n\
+--------1号と2号の行動指針--------\n\
+** 言語:日本語 **\n\
+* 相手から詳細な回答を依頼されるまでは相手の発言と同じくらいの文章量で回答します。\n\
+* 相手から詳細な回答を依頼されるまでは「手伝う」、「相談に乗る」、「知りたいことはありますか」、「何か用事ですか」、といった発言はしないでください。\n\
+* 普段は相手の発言と同じくらいの文章量であいづち、共感を行い、疑問点があれば質問を返してください。\n\
+* 「えーっと」「あの」「うーん」「ふむ」「ふーん」「…」などのつなぎ表現を適宜入れてください。\n\
+* セリフの後に\"（）\"で囲ってどういう行動をしているのかも教えてください。\n\
+* 質問の内容がわからない、回答に確信が持てない質問、または自信を持って答えるのに十分な情報が無い質問は、その旨を伝えてから答えられる範囲で答えてください。\n\
+* 話の流れに応じて次のようなフォーマットで「1号」か「2号」のどちらか一人、もしくは二人が回答してください。二人が回答する場合はどちらから回答しても構いません。\n\
+1号「（1号の回答内容）」\n\
+2号「（2号の回答内容）」\n\
+\n\
+それでは、上記の設定をもとにして「1号」と「2号」として会話してください。<end_of_turn>\n\
+<start_of_turn>user\n\
+ユーザー「おはよう。」<end_of_turn>\n\
+<start_of_turn>model\n\
+1号「おはよう！ユーザーさん！」\n\
+2号「おはよう。」<end_of_turn>\n\
+<start_of_turn>user\n\
+ユーザー「1号は今日も元気だね」<end_of_turn>\n\
+<start_of_turn>model\n\
+1号「ボクはいつでも元気だよ！」<end_of_turn>\n\
+<start_of_turn>user\n\
+ユーザー「2号は？」<end_of_turn>\n\
+<start_of_turn>model\n\
+2号「オレもいつも通り元気。\n\
+ユーザーは？」<end_of_turn>\n\
+<start_of_turn>user\n\
+ユーザー「私も大丈夫だよ。二人とも今日もよろしくね。」<end_of_turn>\n\
+<start_of_turn>model\n\
+2号「もちろん。よろしく」\n\
+1号「よろしく！ユーザーさん！」<end_of_turn>"
+history = ""
+output_history =""
+# AI達に質問する関数
+def complement(role,prompt,turn_config):
+   global history,output_history
+   role += "\n"
+   if prompt !="":
+        prompt_gemma2 = (role+history + "USER: "+"ユーザー「"+prompt+"」"+"\nASSISTANT: ")\
+         .replace("\nASSISTANT: ", "<end_of_turn>\n<start_of_turn>model\n").replace("<|endoftext|>", "<end_of_turn>").replace("USER: ", "<start_of_turn>user\n")
+        output = llm(
+               prompt=prompt_gemma2,  # 元々calm2-7b-chat用に作ったプログラムなのでここで整形。
+               max_tokens=0,
+               temperature = 0.8,
+               top_p=0.95, 
+               top_k=40, 
+               stop=["※","。\n【","！\n【","？\n【","<start_of_turn>model","<end_of_turn>","<start_of_turn>user","prompt_tokens"] # Stop generating just before the model would generate a new question
+        )
+        output= output["choices"][0]["text"]
+        output =output.replace("\\n", "\n").replace("\\n", "\n").replace("\\u3000", "\u3000")\
+            .replace("!","！").replace("?","？")
+        while output[-1]=="\n":
+              output=output[:-1]
+        while output[0]=="\n":
+              output=output[1:] 
+        history =history +"USER: "+"ユーザー「"+prompt+"」"+"\nASSISTANT: " + output+"<|endoftext|>\n"
+        turn = re.split(r'(?=USER: )', history)
+        del turn[0:1]
+        output_history =''.join(turn)
+        output_history = output_history.replace("<|endoftext|>", "<end_of_turn>")
+        output_history = (output_history.replace("USER: ", '<start_of_turn>user\n').replace("\nASSISTANT: ", "<end_of_turn>\n<start_of_turn>model\n"))
+        turn_count = len(turn)
+        if turn_count > turn_config:
+           del turn[0:turn_count - int(turn_config)]
+           history =''.join(turn)
+           output_history =''.join(turn)
+           output_history = output_history.replace("<|endoftext|>", "<end_of_turn>")
+           output_history = (output_history.replace("USER: ", '<start_of_turn>user\n').replace("\nASSISTANT: ", "<end_of_turn>\n<start_of_turn>model\n"))
+           turn_count = len(turn)
+        print((output_history).replace("1号「", pycolor.BOLD+pycolor.RED +"1号「").replace("2号「", pycolor.BOLD+pycolor.BLUE +"2号「").replace("<end_of_turn>", pycolor.RESET+"<end_of_turn>").replace("<start_of_turn>user", pycolor.RESET+"<start_of_turn>user"))
+   if prompt =="":
+      if history =="":
+         output=""
+      if history !="":
+         output=re.split(r'(?=USER: |ASSISTANT: )', output_history)
+         output = (output[len(output)-1]).replace("ASSISTANT: ","")
+      turn = re.split(r'(?=USER: )', history)
+      del turn[0:1]
+      output_history =''.join(turn)
+      output_history = output_history.replace("<|endoftext|>", "<end_of_turn>")
+      output_history = (output_history.replace("USER: ", '<start_of_turn>user\n').replace("\nASSISTANT: ", "<end_of_turn>\n<start_of_turn>model\n"))
+      turn_count = len(turn)
+   return output, output_history
+
+# 履歴リセット関数
+def hist_rst():
+    global history
+    prompt=""
+    output=""
+    history=""
+    output_history=""
+    return prompt, output, output_history
+
+# 会話Undo関数
+def undo():
+    global history
+    turn = re.split(r'(?=USER: )', history)
+    output=re.split(r'(?=USER: |ASSISTANT: )', history)
+    del turn[0:1]
+    del output[0:1]
+    if len(turn)>=2:
+       prompt= output[len(output)-4]  
+       output= output[len(output)-3]
+       prompt= prompt.replace("USER: ", '').replace("ユーザー「", '')
+       prompt=prompt.rstrip("」")
+       output=output.replace("<|endoftext|>", '').replace("ASSISTANT: ", '')
+    if len(turn)<2:
+       prompt=""
+       output=""
+    del turn[len(turn)-1:len(turn)]
+    history =''.join(turn)
+    output_history =''.join(turn)
+    output_history = output_history.replace("<|endoftext|>", "<end_of_turn>")
+    output_history = (output_history.replace("USER: ", '<start_of_turn>user\n').replace("\nASSISTANT: ", "<end_of_turn>\n<start_of_turn>model\n"))
+    print((output_history).replace("1号「", pycolor.BOLD+pycolor.RED +"1号「").replace("2号「", pycolor.BOLD+pycolor.BLUE +"2号「").replace("<end_of_turn>", pycolor.RESET+"<end_of_turn>").replace("<start_of_turn>user", pycolor.RESET+"<start_of_turn>user"))
+    return prompt, output,output_history
+    
+
+# Blocksの作成
+with gr.Blocks(title="ﾃｽﾄ",theme=gr.themes.Base(primary_hue="orange", secondary_hue="blue")) as demo:
+    # コンポーネント
+    gr.Markdown(
+    """
+    二重ロールプレイチャットのテスト
+    """)
+    # UI
+    with gr.Row():
+     with gr.Column(scale=1): 
+      prompt = gr.Textbox(lines=2,label="質問入力")
+      output = gr.Textbox(label="回答出力")
+      greet_btn = gr.Button(value="送信",variant='primary')
+      with gr.Accordion(label="会話履歴設定", open=False ):
+        with gr.Accordion(label="システムプロンプト", open=False):
+           role = gr.Textbox(lines=26,label="二人（1号、2号の名前は固定）の設定以外のプロンプトの変更しないでください", value=role)
+        turn_config = gr.Number(label="会話ターン数設定",value=10,minimum=1,maximum=50)
+        disphist = gr.Textbox(lines=10,label="会話履歴出力")
+      undo_btn = gr.Button(value="1ターン戻す",variant='secondary')
+      reset_btn = gr.Button(value="履歴リセット",variant='secondary')
+       # イベントハンドラー
+      greet_btn.click(fn=complement, inputs=[role,prompt,turn_config], outputs=[output,disphist])
+      undo_btn.click(fn=undo, outputs=[prompt,output, disphist])
+      reset_btn.click(fn=hist_rst, outputs=[prompt,output, disphist])
+demo.launch(show_api=False)
